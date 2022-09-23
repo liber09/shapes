5c3e41b6896f0309a37bfbc2146333e3b9f1b8ce
liber09 <113246195+liber09@users.noreply.github.com>
2022-09-23 21:38:24 +0200

    added missing comments

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,8 +1,10 @@
 public class Rectangle extends Shape{
 
+    //Creates an object of type Rectangle
     public Rectangle(double width,double height){
         super(width,height);
     }
+    //uses superclass shape to return double area
     public double getArea(){
         return super.getArea();
     }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,8 +1,12 @@
 public class Triangle extends Shape {
 
+    //Creates an object of typ Triangle
     public Triangle(double width,double height){
         super(width,height);
     }
+    //Uses superclass shape to calculate area,
+    //divides that area by two to get a triangle area.
+    //returns double area
     public double getArea(){
         return super.getArea()/2;
     }
